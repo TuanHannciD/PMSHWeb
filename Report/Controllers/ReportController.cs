3fa95110703945d1615582354ef512b48de0619d
VuPhatDat <aeslehc19102002@gmail.com>
2025-06-19 17:00:21 +0700

    remove @Scripts RevenueReport

--- Report/Controllers/ReportController.cs
+++ Report/Controllers/ReportController.cs
@@ -1964,6 +1964,7 @@ namespace Report.Controllers
                 case "RoomMoves":
 
                 case "NoShowReport":
+                case "RevenueReports":
                 case "GuestMarketReport":
                 case "LeadtimeReports":
                 case "RatecodeReports":
