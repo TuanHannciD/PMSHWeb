9dee19c814d6669fb3ae159d5f106c12a1cb31ad
namby67 <levuhoang421@gmail.com>
2025-07-16 11:44:22 +0700

    upp cs

--- HouseKeeping/Services/Interfaces/IHouseKeepingService.cs
+++ HouseKeeping/Services/Interfaces/IHouseKeepingService.cs
@@ -12,5 +12,6 @@ namespace HouseKeeping.Services.Interfaces
         DataTable RoomControlPanelData(DateTime fromDate, DateTime toDate, string zone);
         DataTable RoomFacilityForecastData(DateTime fromDate, DateTime toDate, string zone);
         DataTable RoomStatusData(int cleannon_room, int clean, int dirty, int pickup, int oocheck, int oscheck, int vacant, int occupied, int arrivals, int arrived, int stayover, int dayuse, int dueout, int departed, int notReserved, int departuredarr, string roomType, string zone, string roomFrom, string roomTo);
+        DataTable CheckLogStatus(string RoomNo);
     }
 }
