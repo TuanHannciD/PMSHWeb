d4dc69dfad7aee7a4ae187122d8dd45da4b8dbc2
VuPhatDat <aeslehc19102002@gmail.com>
2025-07-21 10:33:16 +0700

    update option in reservation form

---
+++ BaseBusiness/BO/RegistrationCardBO.cs
@@ -0,0 +1,26 @@
+﻿using BaseBusiness.bc;
+using BaseBusiness.Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseBusiness.BO
+{
+    public class RegistrationCardBO : BaseBO
+    {
+        private RegistrationCardFacade facade = RegistrationCardFacade.Instance;
+        protected static RegistrationCardBO instance = new RegistrationCardBO();
+
+        protected RegistrationCardBO()
+        {
+            this.baseFacade = facade;
+        }
+
+        public static RegistrationCardBO Instance
+        {
+            get { return instance; }
+        }
+    }
+}
