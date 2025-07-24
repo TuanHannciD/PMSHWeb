406d9bca029600fb7e98b0d0fd3f92b4b3b33500
namby67 <levuhoang421@gmail.com>
2025-07-24 10:53:07 +0700

    upp bỏder..

--- BaseBusiness/BO/RoomBO.cs
+++ BaseBusiness/BO/RoomBO.cs
@@ -1,5 +1,6 @@
 ﻿using BaseBusiness.bc;
 using BaseBusiness.Facade;
+using BaseBusiness.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,5 +23,22 @@ namespace BaseBusiness.BO
         {
             get { return instance; }
         }
+        public static List<RoomModel> GetRoomZone(string  roomtype, string zone)
+        {
+            int roomtypeInt = int.TryParse(roomtype, out var temp) ? temp : 0;
+            // Chuyển zone thành chuỗi "1,2,3" => "'1','2','3'" (nếu là chuỗi)
+            var zoneList = zone.Split(',').Select(z => $"'{z.Trim()}'");
+            var zoneInClause = string.Join(",", zoneList);
+
+            string query = $@"
+        SELECT count(ID) as zone
+        FROM Room WITH(NOLOCK) 
+        WHERE ({roomtypeInt} = 0 OR RoomTypeID = {roomtypeInt})
+          AND (ZoneID IN ({zoneInClause}) OR '{zone}' = '0')  AND HKStatusID = 7";
+
+            return instance.GetList<RoomModel>(query);
+        }
+
+
     }
 }
