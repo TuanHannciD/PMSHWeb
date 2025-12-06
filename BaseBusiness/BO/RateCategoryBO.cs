f6ff3b86a9c9d00d1b02fe7a4d4723ae2e92b05a
phuclx204 <lxp18001@gmail.com>
2025-12-06 12:04:33 +0700

    pmshweb

---
+++ BaseBusiness/BO/RateCategoryBO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseBusiness.bc;
+using BaseBusiness.Facade;
+using BaseBusiness.Model;
+using Microsoft.Data.SqlClient;
+
+namespace BaseBusiness.BO
+{
+    using Dapper;
+
+    public class RateCategoryBO : BaseBO
+    {
+        private RateCategoryFacade facade = RateCategoryFacade.Instance;
+        protected static RateCategoryBO instance = new RateCategoryBO();
+
+
+        protected RateCategoryBO()
+        {
+            this.baseFacade = facade;
+        }
+
+        public static RateCategoryBO Instance
+        {
+            get { return instance; }
+        }
+
+        public long InsertRateCategory(RateCategoryModel model)
+        {
+            return facade.Insert(model);
+        }
+        public long UpdateRateCategory(RateCategoryModel model)
+        {
+            return facade.Update(model);
+        }
+    }
+}
---
+++ BaseBusiness/Facade/RateCategoryFacade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseBusiness.Model;
+using BaseBusiness.bc;
+
+namespace BaseBusiness.Facade
+{
+    public class RateCategoryFacade : BaseFacadeDB
+    {
+        protected static RateCategoryFacade instance = new RateCategoryFacade(new RateCategoryModel());
+        protected RateCategoryFacade(RateCategoryModel model) : base(model)
+        {
+        }
+        public static RateCategoryFacade Instance
+        {
+            get { return instance; }
+        }
+        protected RateCategoryFacade() : base()
+        {
+        }
+    }
+}
