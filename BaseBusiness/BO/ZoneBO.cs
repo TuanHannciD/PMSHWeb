a894e1b1d4b922f1d57855b5f3640f33c367b796
TuanHannciD <doba2311@gmail.com>
2025-12-29 11:19:54 +0700

    format

--- BaseBusiness/BO/ZoneBO.cs
+++ BaseBusiness/BO/ZoneBO.cs
@@ -24,14 +24,14 @@ namespace BaseBusiness.BO
         public static ZoneBO Instance
         {
             get { return instance; }
-        
+
         }
         public ZoneModel GetById(int id, SqlConnection conn, SqlTransaction tx)
         {
             const string sql = "SELECT ID, Code, Name, Description, CreatedBy, CreatedDate,  UpdatedBy, UpdatedDate FROM Zone WHERE ID = @id";
             return conn.QuerySingleOrDefault<ZoneModel>(sql, new { id }, tx);
         }
-        public static List<RoomAvailabilitySummaryDTO> TotalRoomNight(string  code )
+        public static List<RoomAvailabilitySummaryDTO> TotalRoomNight(string code)
         {
 
             string query = $"SELECT TotalRoomNight, TotalAvail FROM dbo.Zone where Code IN (" + code + ")";
